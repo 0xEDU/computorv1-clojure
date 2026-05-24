740f25bd0e4ac4ed0839ffa7d3939eca315c409e
Eduardo Tachotte <etachott@student.42sp.org.br>
2026-05-24 15:36:22 -0300

    Add tests for lexer

---
+++ test/lexer-test.clj
@@ -0,0 +1,40 @@
+(ns computorv1.lexer-test
+  (:require [clojure.test :as t]
+            [computorv1.lexer :as lexer]))
+
+(t/deftest lexer-test
+  (t/testing "parsing a basic equation"
+    (t/is (= {:tokens [{:type :number :value 5}
+                       {:type :plus}
+                       {:type :number :value 4}
+                       {:type :star}
+                       {:type :indeterminate}
+                       {:type :plus}
+                       {:type :indeterminate}
+                       {:type :caret}
+                       {:type :number :value 2}
+                       {:type :equals}
+                       {:type :indeterminate}
+                       {:type :caret}
+                       {:type :number :value 2}]}
+             (lexer/tokenize "5 + 4 * X + X^2= X^2")))
+
+  (t/testing "parsing an equation with more numbers"
+    (t/is (= {:tokens [{:type :number :value 15}
+                       {:type :minus}
+                       {:type :number :value 42}
+                       {:type :star}
+                       {:type :indeterminate}
+                       {:type :plus}
+                       {:type :indeterminate}
+                       {:type :caret}
+                       {:type :number :value 2}
+                       {:type :equals}
+                       {:type :number :value 35}
+                       {:type :slash}
+                       {:type :indeterminate}
+                       {:type :caret}
+                       {:type :number :value 2}]}
+             (lexer/tokenize "15 - 42 * X + X^2= 35 / X^2"))))))
+
+(t/run-tests 'computorv1.lexer-test)
