49d79ee8e6ef943d564a933f03df6f0fac1efd75
Eduardo Tachotte <bfwaend@gmail.com>
2026-05-24 02:02:19 -0300

    Proud of myself in this one

--- src/computorv1/lexer.clj
+++ src/computorv1/lexer.clj
@@ -1,5 +1,36 @@
 (ns computorv1.lexer)
 
+(def decimal-base 10)
+(def indeterminate \x)
+
+(defn is-number?
+  [c]
+  (not (== (Character/digit c decimal-base) -1)))
+
+(defn classify-char
+  [c]
+  (cond
+    (is-number? c) [:number c]
+    (= c indeterminate) [:indeterminate]
+
+    (= c \+) [:plus]
+    (= c \-) [:minus]
+    (= c \/) [:slash]
+    (= c \*) [:star]
+    (= c \=) [:equals]
+
+    (= c \space) []
+
+    :else [:unknown]))
+
+(defn generate-tokens
+  [state c]
+  (let [token (classify-char c)]
+    (update state :tokens #(conj % token))))
+
 (defn tokenize
   [equation]
-  (println equation))
+  (let [equation (seq equation)]
+    (->> equation
+         (reduce generate-tokens {:tokens []})
+         (println))))
