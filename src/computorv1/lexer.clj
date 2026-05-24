b45280783909bd7e53838b9923495f59d2b42109
Eduardo Tachotte <etachott@student.42sp.org.br>
2026-05-24 15:36:14 -0300

    Improve lexer token structure

--- src/computorv1/lexer.clj
+++ src/computorv1/lexer.clj
@@ -1,36 +1,49 @@
 (ns computorv1.lexer)
 
 (def decimal-base 10)
-(def indeterminate \x)
+(def indeterminate \X)
 
 (defn is-number?
   [c]
   (not (== (Character/digit c decimal-base) -1)))
 
+(defn char-to-number
+  [c]
+  (- (int c) 48))
+
 (defn classify-char
   [c]
   (cond
-    (is-number? c) [:number c]
-    (= c indeterminate) [:indeterminate]
+    (is-number? c) {:type :number
+                    :value (char-to-number c)}
+    (= c indeterminate) {:type :indeterminate}
 
-    (= c \+) [:plus]
-    (= c \-) [:minus]
-    (= c \/) [:slash]
-    (= c \*) [:star]
-    (= c \=) [:equals]
+    (= c \+) {:type :plus}
+    (= c \-) {:type :minus}
+    (= c \/) {:type :slash}
+    (= c \*) {:type :star}
+    (= c \=) {:type :equals}
+    (= c \^) {:type :caret}
 
-    (= c \space) []
+    (= c \space) nil
 
-    :else [:unknown]))
+    :else {:type :unknown}))
+
+(defn conj-if-not-empty
+  [tokens token]
+  (if (empty? token)
+    tokens
+    (conj tokens token)))
 
 (defn generate-tokens
   [state c]
   (let [token (classify-char c)]
-    (update state :tokens #(conj % token))))
+    (update state :tokens #(conj-if-not-empty % token))))
 
 (defn tokenize
   [equation]
   (let [equation (seq equation)]
     (->> equation
-         (reduce generate-tokens {:tokens []})
-         (println))))
+         (reduce generate-tokens {:tokens []}))))
+
+
